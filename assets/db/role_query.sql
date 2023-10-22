19948c67c3b0cedc3c12c3c642b0519696e3938b
Pam <pwillisop@yahoo.com>
2023-10-22 10:08:03 -0400

    removed tests from assets/db

--- assets/db/role_query.sql
+++
@@ -1,3 +0,0 @@
-USE employeeTracker_db;
-
-SELECT * FROM role;
--- assets/db/test_query.sql
+++
@@ -1,8 +0,0 @@
-USE employeeTracker_db;
-
-
-INSERT INTO department(name) VALUES ("Janitorial");
-INSERT INTO role(title, salary) VALUES ("Legal", 45000);
-INSERT INTO employee(first_name, last_name, managerID) VALUES ("fname", "lname", 2);
-JOIN department.id ON role = department.roleID;
-JOIN role.id ON employee = role.employeeID;
